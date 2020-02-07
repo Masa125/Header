ecd14e7ea3c6f70a6f92df0cfb9f68d0c94b80f4
Masa125 <masashi.barth@rbe.sk.ca>
2020-02-07 08:53:46 -0600

    Create numbers.py
    Adding a header and uploaing to GitHub

---
+++ numbers.py
@@ -0,0 +1,14 @@
+# Course: CS 30
+# Period: 1
+# Date created: 20/02/05
+# Date last modified: 20/02/07
+# Name: Masa Barth
+# Description: Numbers that get to 8 and prints variable assigned to 8
+
+
+print(4+4)  # Add 4+4
+print(12-4)  # Subtract 12-4
+print(4*2)  # Multiply 4*2
+print(80/10)  # Divide 80/10
+favourite_number = 8  # 8 is assigned to variable called "favourite_number".
+print(favourite_number)  # Print "favourite_number"
